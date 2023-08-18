c861c7bdd8cb045e6dccc9bcf819307ae806fdaa
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-08-18 13:49:22 +0700

    responsive navbar

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,15 +12,22 @@ const Home = () => {
     })
     return ( 
         
-        <div className="home-container ">
-            <div className="home flex justify-center px-10 md:grid-cols-2 sm:grid-cols-2">
+        <div className="home-container  ">
+            <div className="home lg:grid grid-cols-2  md:grid-cols-2 sm:grid-cols-1 justify-items-center px-10">
+                <div className="">
+                    <div className="photo border-x-4 border-y-4 border-col7 py-10 flex justify-center">
+                        <div className="avatar flex justify-center items-center">
+                            <img src="../avatar2.jpg" className="overflow-hidden object-cover" alt="avatar" />
+                        </div>
+                    </div>
+                </div>
+                
                 <div className="context py-5">
                     <h1 className="text-4xl text-col6 py-4">Hi 👋, I am <span className="rounded-lg px-4">Leap Chanvuthy</span></h1>
                     <h2 className="text-4xl text-col6 pt-4">I am a </h2>
                     <span ><h2 className="text-4xl text-col7 pt-2">{text}<Cursor/></h2></span>
-                    
                     <h2 className="pt-16 text-col3 text-3xl">Contact me</h2>
-                    <ul className="icons flex justify-start">
+                    <ul className="icons flex justify-start ">
                         <li className="pr-6 py-4"> <FaFacebookSquare className="text-3xl  text-col3" />  </li>
                         <li className="pr-6 py-4"> <FaTelegramPlane  className="text-3xl  text-col3"/></li>
                         <li className="pr-6 py-4"> <FaInstagramSquare className="text-3xl  text-col3"/></li>
@@ -32,21 +39,7 @@ const Home = () => {
                         <li><a href="http://t.me/Elon_Thy" className="text-col1 bg-col7 px-7 rounded-md">HIRE ME</a></li>
                     </ul>
                 </div>
-
-                <div className="photo border-x-4 border-y-4 border-col7 py-5 flex justify-center">
-                    <div className="avatar flex justify-center items-center">
-                        <img src="../avatar2.jpg" className="overflow-hidden object-cover" alt="avatar" />
-                    </div>
-                </div>
-            </div>
-
-            {/* about me layout */}
-
-            <div className="about_me flex justify-between px-10 py-20">
-
-            </div>
-
-           
+            </div>           
         </div>
      );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,58 @@
 import { Link } from "react-router-dom";
-import {SiJavascript} from 'react-icons/si'
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {BiCodeAlt} from 'react-icons/bi';
+import { useState } from "react";
+import {HiX} from 'react-icons/hi';
 const Navbar = () => {
-    return ( 
-        <div className="navbar flex justify-between px-40 py-5 font-extrabold">
-            <h3 className="text-2xl gap-4 text-col3">Elon Thy</h3>
-            <ul className="text-col7 text-lg">
-            <Link to="/" className="px-5">Home</Link>
-            <Link to="/blog" className="px-5">Blogs</Link>
-            <Link to="contact" className="px-5">Contact</Link>
-            <Link to="/project" className="px-5">Projects</Link>
-            <Link to="/achievement" className="px-5">Achievements</Link>
-            </ul>
-        </div>
+    const [openNavBar , setOpenNavBar] = useState(false);
+    const toggleNav = () => {
+        setOpenNavBar(!openNavBar);
+    }
+
+    return (
+        <div>
+            <div class="navbar lg:flex justify-center gap-28 px-10 py-10 hidden sm:hidden md:hidden">
+                <div className="flex justify-between items-center gap-5">
+                    <Link to='/'><h3 className="text-2xl text-col3">Elon Thy</h3></Link>
+                    <BiCodeAlt className="text-col3 text-4xl" />
+                </div>
+                <ul className="text-col7 text-lg">
+                    <Link to="/" className="px-5 hover:text-col6 hover:border-b-2">Home</Link>
+                    <Link to="/blog" className="px-5 hover:text-col6 hover:border-b-2">Blogs</Link>
+                    <Link to="contact" className="px-5 hover:text-col6 hover:border-b-2">Contact</Link>
+                    <Link to="/project" className="px-5 hover:text-col6 hover:border-b-2">Projects</Link>
+                    <Link to="/achievement" className="px-5 hover:text-col6 hover:border-b-2">Achievements</Link>
+                </ul>
+            </div>
+            {/* 2nd navbar */}
+            <div className="lg:hidden navbar pb-10">
+                <div className=" flex justify-between px-5 py-8 items-center">
+                    <div className="flex justify-between items-center gap-5">
+                        <Link to='/'><h3 className="text-2xl text-col3">Elon Thy</h3></Link>
+                        <BiCodeAlt className="text-col3 text-3xl font-bold " />
+                    </div>
+                    {openNavBar ? (
+                    <HiX
+                        className="text-col7 text-3xl cursor-pointer"
+                        onClick={toggleNav}
+                    />
+                    ) : (
+                    <GiHamburgerMenu
+                        className="text-col7 text-3xl cursor-pointer"
+                        onClick={toggleNav}
+                    />
+                    )}
+                </div>
+                <ul className={`${openNavBar ? 'grid grid-cols-1' : 'hidden '} text-col3 text-lg justify-items-center bg-col7 rounded-lg px-2 py-4 gap-3 transition duration-1000 ease-in-out`}>
+                    <Link to="/" className="px-5 hover:text-col6 hover:border-b-2">Home</Link>
+                    <Link to="/blog" className="px-5 hover:text-col6 hover:border-b-2"> Blogs</Link>
+                    <Link to="contact" className="px-5 hover:text-col6 hover:border-b-2">Contact</Link>
+                    <Link to="/project" className="px-5 hover:text-col6 hover:border-b-2">Projects</Link>
+                    <Link to="/achievement" className="px-5 hover:text-col6 hover:border-b-2">Achievements</Link>
+                </ul>
+            </div>
+        </div> 
+
      );
 }
  
